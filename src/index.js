287d1b3316cc82b46455a27d9df0d5956e1f80a6
Morris Li <morrislli@outlook.com>
2025-10-09 19:01:24 -0700

    in progress work to migrate to another machine

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
 //import 'bootstrap';
 
-function generateFeaturedTask(taskObj) {
-
-}
+const loginWindow = document.querySelector('#login-window');
