6e6a0b3f99f10eda937cc9f0e36899b29dcff36c
Morris L <morrislli@outlook>
2025-12-01 22:17:34 -0800

    changed express server to allow for bare imports in js sources

--- server.js
+++ server.js
@@ -1,32 +1,61 @@
 import express from "express";
 import path from "path";
+import fs from "fs/promises";
 
 const server = express();
-
-server.use(express.static("./public/"));
-server.use(express.static("./src/"));
 
 server.get("/", (req, res) => {
     res.header("Allow-Access-Control-Origin", "*")
         .type("html").sendFile(path.resolve("./src/landingpage.html"));
 });
 
-// Access page scripts
-server.get("/js/:script", (req, res) => {
-    res.header("Allow-Access-Control-Origin", "*")
-        .sendFile(path.resolve(path.join("./src/js/", req.params.script)));
+// serve html files
+server.get(/^.+\.html$/, (req, res) => {
+    const filenameRe = /\w+\.html$/;
+    let match = req.originalUrl.match(filenameRe);
+    if (match != null) {
+        res.type("text/html").sendFile(
+            path.resolve(path.join("./src", match[0]))
+        );
+    } else {
+        res.status(403).end();
+    }
 });
 
-// Access library scripts
-server.get("/lib/:script", (req, res) => {
-    res.header("Allow-Access-Control-Origin", "*")
-        .sendFile(path.resolve(path.join("./src/lib/", req.params.script)));
+// serve library js
+server.get(/lib\/\w+\.js/, (req, res) => {
+    const jsFile = req.originalUrl.match(/\w+\.js/);
+    fs.readFile(path.resolve(path.join("./src/lib", jsFile[0])))
+        .then(async buff => buff.toString("utf8"))
+        .then(jsText => {
+            jsText = replaceBareImports(jsText);
+            res.type("text/javascript").send(jsText);
+        });
 });
 
-// Calendar app script accesc
-server.get("/scripts/:script", (req, res) => {
-    res.sendFile(path.resolve(path.join("./src/js/", req.params.script)));
+// serve frontend js
+server.get(/js\/\w+\.js/, (req, res) => {
+    const jsFile = req.originalUrl.match(/\w+\.js/);
+    fs.readFile(path.resolve(path.join("./src/js", jsFile[0])))
+        .then(async buff => buff.toString("utf8"))
+        .then(jsText => {
+            jsText = replaceBareImports(jsText);
+            res.type("text/javascript").send(jsText);
+        });
 });
 
+/**
+ * Replaces the node-only firebase/* bare imports in Javascript to Google CDN links for compatibility with espress environments.
+ * @param {String} jsdata String from a Javascript file.
+ * @returns Script string with bare imports replaced with CDN URLs.
+ */
+function replaceBareImports(jsdata) {
+    jsdata = jsdata.replace("firebase/app", "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js");
+    jsdata = jsdata.replace("firebase/auth", "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js");
+    jsdata = jsdata.replace("firebase/firestore", "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js");
+    return jsdata;
+}
+
+server.use(express.static("./public/"));
 
 server.listen(3000);
